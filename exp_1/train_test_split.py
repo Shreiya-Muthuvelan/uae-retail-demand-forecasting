df74f4cb80ff6e2681e6001fa2498d82d9ea1db6
Shreiya Muthuvelan <shreiyamuthuvelan@gmail.com>
2026-09-04 15:16:04 +0400

    Refactor train_test_split for clarity and simplicity
    Refactored train_test_split function to simplify code and improve readability.

--- exp_1/train_test_split.py
+++ exp_1/train_test_split.py
@@ -7,37 +7,12 @@ def train_test_split_for_time_series(dataset):
     """
 
     cutoff_date = dataset["Week"].quantile(0.8)
+    train_set = (dataset[dataset["Week"] <= cutoff_date].copy())
+    test_set = ( dataset[dataset["Week"] > cutoff_date].copy())
 
-    train_set = (
-        dataset[
-            dataset["Week"] <= cutoff_date
-        ]
-        .copy()
-    )
-
-    test_set = (
-        dataset[
-            dataset["Week"] > cutoff_date
-        ]
-        .copy()
-    )
-
-    print("\n===== Time-Based Split =====")
-
+    print("\n Time-Based Split ")
     print("Cutoff:", cutoff_date)
-
-    print(
-        "Train:",
-        train_set["Week"].min(),
-        "→",
-        train_set["Week"].max()
-    )
-
-    print(
-        "Test:",
-        test_set["Week"].min(),
-        "→",
-        test_set["Week"].max()
-    )
+    print("Train:",train_set["Week"].min(),"→",train_set["Week"].max())
+    print("Test:",test_set["Week"].min(), "→",test_set["Week"].max())
 
     return train_set, test_set
